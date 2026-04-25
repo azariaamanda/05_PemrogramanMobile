47f6ec5b7d65d4e6f472c36632db9e7150d36ab0
azaria amanda <azaryaamanda@gmail.com>
2026-04-25 13:54:42 +0700

    Pertemuan6

--- Pertemuan6/README.md
+++ Pertemuan6/README.md
@@ -17,3 +17,17 @@ Absen   : 05 <br>
 4. Langkah 4: Implementasi title row<br>
 ![prak1langkah4](img/Prak1/Prak1Langkah4.png)<br>
 ![prak1langkah4](img/Prak1/Prak1Langkah4_hasil.png)<br>
+
+## Praktikum 2: Implementasi button row
+1. Langkah 1: Buat method Column _buildButtonColumn<br>
+- lib/main.dart (_buildButtonColumn) <br>
+![prak2langkah1](img/prak1/Prak2Langkah1.png)<br>
+
+2. Langkah 2: Buat widget buttonSection<br>
+- lib/main.dart (buttonSection)<br>
+![prak2langkah2](img/prak1/Prak2Langkah2.png)<br>
+
+3. Langkah 3: Tambah button section ke body<br>
+- Tambahkan variabel buttonSection ke dalam body seperti berikut:<br>
+![prak2langkah3](img/prak1/Prak2Langkah3.png)<br>
+![prak2langkah3](img/prak1/Prak2Langkah3_hasil.png)<br>
--- Pertemuan6/layout_flutter/lib/main.dart
+++ Pertemuan6/layout_flutter/lib/main.dart
@@ -44,8 +44,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
-      title: 'Flutter Layout Demo',
+      title: 'Flutter Layout Demo Azaria Amanda/244107060060',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Layout Demo'),
@@ -53,9 +64,31 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
